dbb8d3eb3250df78888cc8f5b1e72ea43681b899
ares27 <jehomasb@gmail.com>
2020-04-09 21:21:45 +0200

    Added MERN dependencies.

--- server.js
+++ server.js
@@ -1,10 +1,44 @@
 const express = require('express')
 const app = express()
-const port = process.env.PORT || 5000;
+const mongoose = require('mongoose')
+const morgan = require('morgan')
+const path = require('path')
 
+//SET PORT
+const PORT = process.env.PORT || 8000;
+
+//use morgan - http logger
+app.use(morgan('tiny'));
+
+
+
+
+
+
+//SERVER ROUTES
 app.get('/', (req, res) => {
-    
-    res.send('Hello World!')
+
+    res.json('Hello World!')
 });
 
-app.listen(port, () => console.log(`Server listening on port: ${port}`))
+app.get('/api', (req, res) => {
+
+    res.json('Hello World!')
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//run server
+app.listen(PORT, () => console.log(`Server listening on port: ${PORT}`))
