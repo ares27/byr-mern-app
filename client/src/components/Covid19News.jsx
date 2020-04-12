731262687b729774a236f8186d0bd0a853ab1b4b
ares27 <jehomasb@gmail.com>
2020-04-12 09:37:02 +0200

    Updated covid19 information.

--- client/src/components/Covid19News.jsx
+++ client/src/components/Covid19News.jsx
@@ -6,18 +6,25 @@ import { Alert, Button } from "reactstrap";
 import { Card, Container, Row, Col } from 'react-bootstrap';
 
 //get images
-import infectedImg from '../assets/sad.png';
+import testsImg from '../assets/tests.png';
+import activeImg from '../assets/active.png';
+import infectedImg from '../assets/infected.png';
+import chartImg from '../assets/chart.png';
+//import infectedImg from '../assets/sad.png';
 import recoveredImg from '../assets/emoji.png';
+import criticalImg from '../assets/dead.png';
 import deathImg from '../assets/death.png';
 
 class COVID19News extends Component {
+    
     
     //contain form values
     state = { 
         title: '',
         cases: null,
         deaths: null,
-        recovered: null
+        recovered: null,
+        covidCases: []
     };
 
     componentDidMount = () => {
@@ -32,7 +39,18 @@ class COVID19News extends Component {
             //this.setState({ posts: data });
             console.log("Data received", data);
             
-            this.setState({ cases: data.cases, deaths: data.deaths, recovered: data.recovered });
+            //create details object
+            let covidData = {
+                totalActive: data.active, 
+                totalCases: data.cases,
+                totalDeaths: data.deaths,
+                totalRecovered: data.recovered,
+                critical: data.critical,
+                tests: data.tests            
+            }
+
+            this.setState({ covidCases: covidData });
+            
         })
         .catch((err)=> {
             //alert("Error receiving data...")
@@ -42,17 +60,16 @@ class COVID19News extends Component {
     }
 
 
+
     render() { 
 
-        console.log("state: ", this.state);
-        const { cases, deaths, recovered } = this.state;
+        //console.log("state: ", this.state);
+        const { totalActive, totalCases, totalDeaths, totalRecovered, critical, tests } = this.state.covidCases;
 
         return (
             
             <Container fluid
             className="p-3">
-
-            
 
                 <Container>
                     <Alert 
@@ -61,31 +78,91 @@ class COVID19News extends Component {
                         COVID-19 data and information sourced at <a href="https://sacoronavirus.co.za/">https://sacoronavirus.co.za/</a>
                     </Alert>
 
-                    <Row className="pl-5">
+                    <Row className="justify-content-center">
                         
-                        <Col md={4}
-                        className="justify-content-center text-center">
+                        <Col md={3}
+                        className="text-center pt-2">
+                            
                             <Card 
-                            style={{ width: '18rem' }}
-                            className="covid19-card">
-                                <Card.Img variant="top" src={infectedImg}/>
+                            style={{ width: '12rem', marginLeft: '20px' }}
+                            className="shadow p-3 mb-5 bg-white rounded
+                            border-warning
+                            covid19-card">
+                                <Card.Img variant="top" src={chartImg}/>
                                 <Card.Body>
                                 <Card.Title
-                                className="card-title"><h2>{cases}</h2></Card.Title>
-                                <Card.Text>INFECTED</Card.Text>
+                                className="card-title"><h2>{totalCases}</h2></Card.Title>
+                                <Card.Text className="card-text">TOTAL CASES</Card.Text>
                                 </Card.Body>
                             </Card>
                         </Col>
                         
-                        <Col md={4}
-                        className="justify-content-center text-center">
-                        <Card style={{ width: '18rem' }}
-                        className="covid19-card">
+                        <Col md={3}
+                        className="text-center pt-2">
+                        <Card style={{ width: '12rem' }}
+                        className="shadow p-3 mb-5 bg-white rounded
+                        border-info
+                        covid19-card">
+                            <Card.Img variant="top" src={testsImg} />
+                            <Card.Body>
+                            <Card.Title 
+                            className="card-title">
+                                <h2>{tests}</h2></Card.Title>
+                                <Card.Text>
+                                TOTAL TESTS
+                                </Card.Text>
+                            </Card.Body>
+                            </Card>
+                        </Col>
+                               
+                        <Col md={3}
+                        className="text-center pt-2">
+                        <Card style={{ width: '12rem'}}
+                        className="shadow p-3 mb-5 bg-white rounded
+                        border-dark
+                        covid19-card">
+                            <Card.Img variant="top" src={activeImg} />
+                            <Card.Body>
+                                <Card.Title
+                                className="card-title">
+                                    <h2>{totalActive}</h2></Card.Title>
+                                <Card.Text>
+                                ACTIVE
+                                </Card.Text>
+                                {/* <Button variant="primary">Go somewhere</Button> */}
+                            </Card.Body>
+                            </Card>
+                        </Col>
+
+                       
+                        
+                        <Col md={3}
+                        className="text-center pt-2">
+                            <Card 
+                            style={{ width: '12rem'}}
+                            className="shadow p-3 mb-5 bg-white rounded
+                            border-warning
+                            covid19-card">
+                                <Card.Img variant="top" src={infectedImg}/>
+                                <Card.Body>
+                                <Card.Title
+                                className="card-title"><h2>{totalCases}</h2></Card.Title>
+                                <Card.Text>INFECTIONS</Card.Text>
+                                </Card.Body>
+                            </Card>
+                        </Col>
+                        
+                        <Col md={3}
+                        className="text-center pt-5">
+                        <Card style={{ width: '12rem' }}
+                        className="shadow p-3 mb-5 bg-white rounded
+                        border-success
+                        covid19-card">
                             <Card.Img variant="top" src={recoveredImg} />
                             <Card.Body>
                                 <Card.Title
                                 className="card-title">
-                                    <h2>{recovered}</h2></Card.Title>
+                                    <h2>{totalRecovered}</h2></Card.Title>
                                 <Card.Text>
                                 RECOVERED
                                 </Card.Text>
@@ -94,24 +171,51 @@ class COVID19News extends Component {
                             </Card>
                         </Col>
 
-                        <Col md={4}
-                        className="justify-content-center text-center">
-                        <Card style={{ width: '18rem' }}
-                        className="covid19-card">
+
+
+                        <Col md={3}
+                        className="text-center pt-5">
+                        <Card style={{ width: '12rem' }}
+                        className="shadow p-3 mb-5 bg-white rounded
+                        border-danger
+                        covid19-card">
+                            <Card.Img variant="top" src={criticalImg} />
+                            <Card.Body>
+                                <Card.Title 
+                                className="card-title"><h2>{critical}</h2></Card.Title>
+                                <Card.Text>
+                                CRITICAL
+                                </Card.Text>
+                            </Card.Body>
+                        </Card>
+                        </Col>
+
+
+                        <Col md={3}
+                        className="text-center pt-5">
+                        <Card style={{ width: '12rem' }}
+                        className="shadow p-3 mb-5 bg-white rounded
+                        border-danger
+                        covid19-card">
                             <Card.Img variant="top" src={deathImg} />
                             <Card.Body>
-        <Card.Title 
-        className="card-title"><h2>{deaths}</h2></Card.Title>
+                                <Card.Title 
+                                className="card-title"><h2>{totalDeaths}</h2></Card.Title>
                                 <Card.Text>
                                 DEAD
                                 </Card.Text>
                             </Card.Body>
-                            </Card>
+                        </Card>
                         </Col>
+
                         
+                        
+
+
                     </Row>
           
                 </Container>
+
             </Container>  
             
 
