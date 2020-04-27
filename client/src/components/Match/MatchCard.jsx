17904c7e520fa35a7b99a316cb3a7be756e9cd50
ares27 <jehomasb@gmail.com>
2020-04-27 23:01:44 +0200

    Updated.

--- client/src/components/Match/MatchCard.jsx
+++ client/src/components/Match/MatchCard.jsx
@@ -3,6 +3,8 @@ import axios from 'axios'
 import { Alert, Card, Container } from 'react-bootstrap'
 import './Match.css'
 import img from '../../assets/za-flag.png'
+import vsImg from '../../assets/vs.png'
+
 import { CardSubtitle } from 'react-bootstrap/Card'
 
 export const MatchCard = () => {
@@ -52,43 +54,49 @@ export const MatchCard = () => {
             <Container className="">
             
 
-                <div className="row">
+                <div className="row flex-container wrap">
                 {
                     cards.map(card => 
-                        <div className="col m-1" key={card._id}>
+                        // <div className="col m-1" key={card._id}>
                             
-                            <Card className="m-1">
+                            <Card key={card._id} id="match-card" className="m-2" >
                                 <div className="card-header text-center text-white bg-secondary">
                                 MATCH CARD
                                 </div>
                                 
                                <Card body>
 
-                                   <div className="row justify-content-between m-1 p-1">
-                                     <div className="col-4 text-center p-1">
-                                         <img src={img} className="rounded profile-img"  />
+                                   <div className="row justify-content-between m-1">
+                                     <div className="col-5 text-center p-1">
+                                         {/* <img src={img} className="rounded profile-img"  /> */}
+                                         <figure class="figure">
+                                            <img src={img} class="figure-img img-fluid rounded w-75" alt="Player-1-img" />
+                                            <figcaption class="figure-caption text-center">{card.player1}</figcaption>
+                                         </figure>
                                      </div>
-                                     <div className="col-4 text-center p-1">
-                                         <img src={img} className="rounded profile-img"  />
+                                     <div className="col-5 text-center p-1">
+                                         {/* <img src={img} className="rounded profile-img"  /> */}
+                                         <figure class="figure">
+                                            <img src={img} class="figure-img img-fluid rounded w-75" alt="Player-1-img" />
+                                            <figcaption class="figure-caption text-center">{card.player2}</figcaption>
+                                         </figure>
                                      </div>
                                  </div>
 
-                                 <div className="row justify-content-between m-1 p-1">
-                                     <div className="col-4 text-center">{card.player1}</div>
-                                     <div className="col-4 text-center">{card.player2}</div>
-                                 </div>
-
-                                 <div className="row justify-content-center m-1 p-1">
-                                     <div className="col-4 text-center">VS</div>
+                             
+                                 <div className="row justify-content-center">
+                                     <div className="col-5 text-center">
+                                        <img src={vsImg} className="img-fluid rounded w-75" alt="vs-img" />
+                                     </div>
                                  </div>
 
                              </Card>
 
                              <Card.Footer className="bg-secondary text-white">
 
-                                     <div className="row justify-content-between m-1 p-1">
-                                             <div className="col-4  text-center">{card.score1}</div>
-                                             <div className="col-4  text-center">{card.score2}</div>
+                                     <div className="row justify-content-between">
+                                             <div className="col-5  text-center">{card.score1}</div>
+                                             <div className="col-5  text-center">{card.score2}</div>
                                      </div>
 
                                  </Card.Footer>
@@ -96,7 +104,7 @@ export const MatchCard = () => {
                          </Card>  
                         
                         
-                        </div>
+                        // </div>
                     )
                 }
                 </div>
